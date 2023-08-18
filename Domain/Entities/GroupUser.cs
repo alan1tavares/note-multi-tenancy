876ab1e568a40c8ac29cd8927bd0d35d7f57ceca
Alan Tavares <alan1tavares@gmail.com>
2023-08-17 23:28:03 -0300

    feat: add as entidades
    Criado os projeto: api, dominio e infraestrutura

---
+++ Domain/Entities/GroupUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class GroupUser
+    {
+        public Guid Id { get; set; }
+        public required Group Group { get; set; }
+        public required User User { get; set; }
+    }
+}
---
+++ Domain/Entities/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Note
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreateDate { get; set; }
+        public required User User { get; set; }
+        public required Group Group { get; set; }
+    }
+}
