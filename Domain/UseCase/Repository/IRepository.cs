09590a321b06439291ebfa75aba3a7e79843558a
Alan Tavares <alan1tavares@gmail.com>
2023-08-17 23:31:06 -0300

    feat: add interface repository

---
+++ Domain/UseCase/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UseCase.Repository
+{
+    internal interface IRepository<E>
+    {
+        public Task<E> SaveAsync(E entity);
+        public void Update(E entity);
+        public E GetById(Guid id);
+        public IEnumerable<E> GetAll();
+        public void Delete(Guid id);
+    }
+}
